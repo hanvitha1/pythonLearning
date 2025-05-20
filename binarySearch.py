be132052997f74cbd881aa637acd5f4286f4e8e4
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-05-20 15:32:29 +0530

    binary search

--- binarySearch.py
+++ binarySearch.py
@@ -1,18 +1,19 @@
-s = 89
+s = 16
 arr = [1, 4, 7, 12, 13, 15, 18]
 low = 0
 high = len(arr) - 1
 flag = False
-while mid>=0:
+while low <= high:
     mid = (low + high) // 2
     if arr[mid] == s:
-        print(mid)
+        flag = True
         break
     elif arr[mid] < s:
         low = mid+1
     elif arr[mid] > s:
         high = mid-1
-if flag == False:
+    mid = low +high // 2
+if flag == True:
     print(f"index {mid}")
 else:
     print("element not found")
