ca22b85962d0b43914bc0f21c1693c23498cda32
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-05-20 13:05:21 +0530

    with note inside

---
+++ xor_swaap.py
@@ -0,0 +1,8 @@
+a = 99
+b = 3
+a = a^b
+b = a^b #here b=(a^b)^b | = a^(b^b) | = a^0  = a . so now b became a
+a = a^b #here a=(a^b)^a (since b became a in the previous step)| = (a^a)^b | 0^b = b
+#both the values of a and be are swapped.
+print(f"a is {a}")
+print(f"b is {b}")
