a6b054f37b0651bc9e6b1282da1d60533a59ba84
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-05-21 11:55:37 +0530

    used function

--- tables.py
+++ tables.py
@@ -1,6 +1,8 @@
-n = int(input("enter n value: "))
-i=1
-while i<=10:
-        multiple = n*i
-        print(f"{n} * {i} = {multiple}")
-        i+=1
+# n = int(input("enter n value: "))
+def table(n):
+        i=1
+        while i<=10:
+                multiple = n*i
+                print(f"{n} * {i} = {multiple}")
+                i+=1
+table(6)
