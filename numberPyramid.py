f84ec0a8e35a182363f1bf46d27ca819fbf8fa73
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-05-23 10:27:25 +0530

    added numberPyramid.py

--- numberPyramid.py
+++ numberPyramid.py
@@ -1,11 +1,17 @@
 row = 5
-num = 189
+num = 10
 for i in range(row):
-    print(" " * ((row-i-1)*4), end=" ")
+    print("   " * (row-i-1), end=" ")
     for j in range (((i+1)*2) - 1):
         print(num, end=" ")
         num += 1
-    print("\n")
+    print()
+for i in range(row-2,-1,-1):
+    print("   " * (row-i-1), end=" ")
+    for j in range (((i+1)*2) - 1):
+        print(num, end=" ")
+        num += 1
+    print()
 
 
 
