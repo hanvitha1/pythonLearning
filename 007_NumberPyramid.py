1dc28dafeec6d8b5eba1b3f659282be217102f72
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-05-29 17:10:29 +0530

    number pyramid

--- 007_NumberPyramid.py
+++ 007_NumberPyramid.py
@@ -1,33 +1,18 @@
 row = 5
 num = 100
-n= ((row)*row)+((row-1)*(row-1))
-print(n)
-print(num + (n-1))
-print(len (str(n)))
-cal = len (str((num)+(n-1)))
-
-# for i in range(row):
-#     for j in range (((i+1)*2) - 1):
-#         num += 1
-# for i in range(row-2,-1,-1):
-#     for j in range (((i+1)*2) - 1):
-#         num += 1
-# cal = len(str(num))
-
 for i in range(row):
-    print(" " * (row-i-1)* (cal+1), end=" ")
-    for j in range (((i+1)*2) - 1):
-        print("0"*(cal-len(str(num))), end= "")
+    print("   " * (row - i - 1), end="")
+    for j in range((i + 1) * 2 - 1):
         print(num, end=" ")
         num += 1
     print()
-for i in range(row-2,-1,-1):
-    print(" " * (row-i-1) * (cal + 1), end=" ")
-    for j in range (((i+1)*2) - 1):
-        print(" "*(cal-len(str(num))), end= "")
-        print(num, end=" ")
-        num += 1
-    print()
+
+
+
+
+
+
+
 
 
 
