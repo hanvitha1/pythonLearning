15a3a1ef9b2670ff70072edcdb58c5d1e5a4efe4
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-05-30 14:39:50 +0530

    fibonacci series

---
+++ 014_Fibonacci.py
@@ -0,0 +1,33 @@
+# def fib(n):
+#     L = [1, 1]
+#     if n == 1:
+#         return L[:1]
+#     elif n == 2:
+#         return L[:2]
+#     else:
+#         while (n - 2) > 0:
+#             # print (L)
+#             L.append(L[-1] + L[-2])
+#             n -= 1
+#         return L
+# for i in range(1, 10):
+#     print(i, fib(i))
+
+n=10
+a=0
+b=1
+i=0
+while i < n:
+    print(a, end = " ")
+    temp = b
+    b=a+b
+    a=temp
+    i = i + 1
+
+
+
+
+
+
+
+
