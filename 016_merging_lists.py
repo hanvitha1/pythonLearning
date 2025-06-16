138dc69dddb7456f649b067a8ec5fee1cdc73df2
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-06-16 10:32:28 +0530

    merging of two lists without duplicates

---
+++ 016_merging_lists.py
@@ -0,0 +1,15 @@
+a = [1, 2, 3, 5, 7]
+b = [2, 3, 4, 6, 8]
+c = a + b
+merged = []
+for i in c:
+    if i not in merged:
+        merged.append(i)
+
+for i in range(len(merged)):
+    for j in range(i + 1, len(merged)):
+        print(i)
+        if merged[i] > merged[j]:
+            merged[i], merged[j] = merged[j], merged[i]
+
+print(merged)
