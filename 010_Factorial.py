3d28b9b83c52eaed77b8c8b714c761ba0a4b77f5
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-06-16 10:28:46 +0530

    factorial with function

--- 010_Factorial.py
+++ 010_Factorial.py
@@ -1,11 +1,22 @@
-def f(n):
-    fact = 1
-    for i in range (1, n+1):
-        fact = fact * i
-    print(fact)
-    return fact
-n = 5
-res = f(n)
+# def f(n):
+#     fact = 1
+#     for i in range (1, n+1):
+#         fact = fact * i
+#     print(fact)
+#     return fact
+# n = 5
+# res = f(n)
 
+
+def fact(n):
+    # fact = 1
+    if n==0:
+        return 1
+    else:
+        return n * fact(n-1)
+
+n=5
+res = fact(n)
+print(res)
 
 
