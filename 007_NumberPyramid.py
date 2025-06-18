dc994d7bb7d2d31f4c75afe59774e3704bac55f6
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-06-18 12:39:21 +0530

    number pyramid

--- 007_NumberPyramid.py
+++ 007_NumberPyramid.py
@@ -1,7 +1,7 @@
 row = 5
 num = 100
 for i in range(row):
-    print("   " * (row - i - 1), end="")
+    print("    " * (row - i - 1), end="")
     for j in range((i + 1) * 2 - 1):
         print(num, end=" ")
         num += 1
