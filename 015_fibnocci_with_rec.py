e1d22dd731f7754d82e3f04b1f4cc7cce08af7a2
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-06-18 12:36:49 +0530

    fibnocci series using recursion

--- 015_fibnocci_with_rec.py
+++ 015_fibnocci_with_rec.py
@@ -1,13 +1,25 @@
-n = 8
-a = 0
-b = 1
+# n = 8
+# a = 0
+# b = 1
+#
+# def fib(n, a, b):
+#     if n == 0:
+#         return b
+#     result = a + b
+#     print(result, end=" ")
+#     return fib(n-1, b, result)
+#
+# print(a, b, end=" ")
+# fib(n-2, a, b)
 
-def fib(n, a, b):
+def fib(n):
     if n == 0:
-        return b
-    result = a + b
-    print(result, end=" ")
-    return fib(n-1, b, result)
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
 
-print(a, b, end=" ")
-fib(n-2, a, b)
+n = 6
+for i in range(n):
+    print(fib(i), end=" ")
