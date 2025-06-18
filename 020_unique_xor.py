e901ec93580db60562e6df413900a5a5fb4fe056
Hanvitha Gownipalli <ghanvitha01@gmail.com>
2025-06-18 12:35:53 +0530

    unique elements in list using xor

--- 020_unique_xor.py
+++ 020_unique_xor.py
@@ -1,5 +1,5 @@
 #unique elements in list using xor
-s = [1,2,3,4,1,2]
+s = [1,2,3,1,2]
 l = []
 for i in range (len(s)):
     count = 0
@@ -10,14 +10,14 @@ for i in range (len(s)):
         l.append(s[i])
 print(l)
 
-#repeating elements in list using xor
-s = [1,2,3,4,1,2]
-l = []
-for i in range (len(s)):
-    count = 0
-    for j in range (len(s)):
-        if i != j and (s[i] ^ s[j]) == 0:
-            count += 1
-    if count > 0 and s[i] not in l:
-        l.append(s[i])
-print(l)
+# #repeating elements in list using xor
+# s = [1,2,3,4,1,2]
+# l = []
+# for i in range (len(s)):
+#     count = 0
+#     for j in range (len(s)):
+#         if i != j and (s[i] ^ s[j]) == 0:
+#             count += 1
+#     if count > 0 and s[i] not in l:
+#         l.append(s[i])
+# print(l)
